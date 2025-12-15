2485f91de554dd6aaaf5b35cf6c11c2a9cc0e3a4
Z3ltu <andersengdahl1989@gmail.com>
2025-12-15 15:58:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bordmester-v2";
+const CACHE = "bordmester-v3";
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll([
@@ -15,3 +15,4 @@ self.addEventListener("activate", e => {
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
+
