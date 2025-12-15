733623eefcbd49a9f7212e710bf2aa7d8a9dd7d8
Z3ltu <andersengdahl1989@gmail.com>
2025-12-15 12:50:50 +0100

    Update service worker caching logic and paths

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
 const CACHE_NAME = "bordmester-cache-v1";
 const urlsToCache = [
-  "index.html",
-  "script.js",
-  "manifest.json",
-  "icon.png"
+  "/bordmester/",
+  "/bordmester/index.html",
+  "/bordmester/script.js",
+  "/bordmester/manifest.json",
+  "/bordmester/icon-192.png",
+  "/bordmester/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
