1b0f10b1ef55cbd85ad32043f8351cd01e3c9116
Z3ltu <andersengdahl1989@gmail.com>
2025-12-15 15:49:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
+const CACHE = "bordmester-v2";
+
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("bordmester-v1").then(c => c.addAll([
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
     "index.html", "script.js", "manifest.json", "icon.png", "icon-192.png", "icon-512.png"
   ])));
 });
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
